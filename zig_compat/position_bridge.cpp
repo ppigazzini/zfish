d9770cccce7e829b3207b8a30e91168b9669590d
ppigazzini <pasquale.pigazzini@gmail.com>
2026-05-18 21:54:48 +0200

    port: inline bitboard bridge into position
    Move the retained bitboard bridge definitions into the position bridge so the
    separate bitboard bridge translation unit can be removed.

--- zig_compat/position_bridge.cpp
+++ zig_compat/position_bridge.cpp
@@ -16,8 +16,14 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <algorithm>
+#include <array>
+#include <bitset>
 #include <cstdlib>
+#include <initializer_list>
 #include <string>
+
+#include "misc.h"
 
 #define ZFISH_POSITION_BRIDGE_SKIP_COMPUTE_MATERIAL_KEY
 #define ZFISH_POSITION_BRIDGE_SKIP_ENDGAME_SET
@@ -55,6 +61,8 @@ const char*   zfish_position_format_fen(const unsigned char* board_ptr,
 std::uint64_t zfish_position_compute_material_key(const int* piece_counts_ptr,
                                                   std::size_t piece_count_len);
 void          zfish_position_init_runtime();
+const char*   zfish_bitboard_pretty(Stockfish::Bitboard bitboard);
+void          zfish_bitboards_init();
 
 std::uint64_t zfish_position_material_zobrist(std::uint8_t piece, std::size_t count_index) {
     return Stockfish::Zobrist::psq[piece][8 + count_index];
@@ -66,6 +74,135 @@ void zfish_position_init_runtime() {
 }
 
 namespace Stockfish {
+
+uint8_t PopCnt16[1 << 16];
+uint8_t SquareDistance[SQUARE_NB][SQUARE_NB];
+
+Bitboard LineBB[SQUARE_NB][SQUARE_NB];
+Bitboard BetweenBB[SQUARE_NB][SQUARE_NB];
+Bitboard RayPassBB[SQUARE_NB][SQUARE_NB];
+
+alignas(64) Magic Magics[SQUARE_NB][2];
+
+namespace {
+
+using MagicMask = Bitboard;
+
+[[maybe_unused]] constexpr Bitboard constexpr_pext(Bitboard b, Bitboard m) {
+    Bitboard result = 0, bit = 0;
+    while (m)
+    {
+        Bitboard last = m & -m;
+        result |= bool(b & last) << bit++;
+        m ^= last;
+    }
+    return result;
+}
+
+void init_magics(PieceType pt, MagicMask table[], Magic magics[][2], [[maybe_unused]] bool tableAlreadyInit) {
+    tableAlreadyInit = false;
+
+    int seeds[][RANK_NB] = {{8977, 44560, 54343, 38998, 5731, 95205, 104912, 17020},
+                            {728, 10316, 55013, 32803, 12281, 15100, 16645, 255}};
+
+    Bitboard occupancy[4096];
+    int      epoch[4096] = {}, cnt = 0;
+    Bitboard reference[4096] = {};
+    int      size = 0;
+
+    for (Square s = SQ_A1; s <= SQ_H8; ++s)
+    {
+        Bitboard edges = ((Rank1BB | Rank8BB) & ~rank_bb(s)) | ((FileABB | FileHBB) & ~file_bb(s));
+
+        Magic&   m       = magics[s][pt - BISHOP];
+        Bitboard attacks = Bitboards::sliding_attack(pt, s, 0);
+        m.mask           = attacks & ~edges;
+        m.shift          = (Is64Bit ? 64 : 32) - popcount(m.mask);
+
+        m.attacks = s == SQ_A1 ? table : magics[s - 1][pt - BISHOP].attacks + size;
+        size      = 0;
+
+        Bitboard b = 0;
+        do
+        {
+            occupancy[size] = b;
+            reference[size] = Bitboards::sliding_attack(pt, s, b);
+
+            size++;
+            b = (b - m.mask) & m.mask;
+        } while (b);
+
+        PRNG rng(seeds[Is64Bit][rank_of(s)]);
+
+        for (int i = 0; i < size;)
+        {
+            for (m.magic = 0; popcount((m.magic * m.mask) >> 56) < 6;)
+                m.magic = rng.sparse_rand<Bitboard>();
+
+            for (++cnt, i = 0; i < size; ++i)
+            {
+                unsigned idx = m.index(occupancy[i]);
+
+                if (epoch[idx] < cnt)
+                {
+                    epoch[idx]     = cnt;
+                    m.attacks[idx] = reference[i];
+                }
+                else if (m.attacks[idx] != reference[i])
+                    break;
+            }
+        }
+    }
+}
+
+std::array<Bitboard, 0x19000> RookTable;
+std::array<Bitboard, 0x1480>  BishopTable;
+
+std::string take_string_and_free_or_empty(const char* rendered) {
+    if (!rendered)
+        return {};
+
+    std::string value(rendered);
+    std::free(const_cast<char*>(rendered));
+    return value;
+}
+
+}  // namespace
+
+namespace Bitboards {
+
+void init() {
+    for (unsigned i = 0; i < (1 << 16); ++i)
+        PopCnt16[i] = uint8_t(std::bitset<16>(i).count());
+
+    for (Square s1 = SQ_A1; s1 <= SQ_H8; ++s1)
+        for (Square s2 = SQ_A1; s2 <= SQ_H8; ++s2)
+            SquareDistance[s1][s2] = std::max(distance<File>(s1, s2), distance<Rank>(s1, s2));
+
+    init_magics(ROOK, const_cast<MagicMask*>(RookTable.data()), Magics, true);
+    init_magics(BISHOP, const_cast<MagicMask*>(BishopTable.data()), Magics, true);
+
+    for (Square s1 = SQ_A1; s1 <= SQ_H8; ++s1)
+    {
+        for (PieceType pt : {BISHOP, ROOK})
+            for (Square s2 = SQ_A1; s2 <= SQ_H8; ++s2)
+            {
+                if (PseudoAttacks[pt][s1] & s2)
+                {
+                    LineBB[s1][s2] = (attacks_bb(pt, s1, 0) & attacks_bb(pt, s2, 0)) | s1 | s2;
+                    BetweenBB[s1][s2] =
+                      (attacks_bb(pt, s1, square_bb(s2)) & attacks_bb(pt, s2, square_bb(s1)));
+                    RayPassBB[s1][s2] =
+                      attacks_bb(pt, s1, 0) & (attacks_bb(pt, s2, square_bb(s1)) | s2);
+                }
+                BetweenBB[s1][s2] |= s2;
+            }
+    }
+}
+
+std::string pretty(Bitboard b) { return take_string_and_free_or_empty(zfish_bitboard_pretty(b)); }
+
+}  // namespace Bitboards
 
 Key Position::compute_material_key() const {
     return zfish_position_compute_material_key(pieceCount, PIECE_NB);
@@ -92,3 +229,7 @@ string Position::fen() const {
 }
 
 }  // namespace Stockfish
+
+extern "C" void zfish_bitboards_init() {
+    Stockfish::Bitboards::init();
+}
